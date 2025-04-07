d589dc491e95843d3b98a0de6fb6d8eabd9d72b0
Jason Reslock <jreslock@users.noreply.github.com>
2025-04-06 21:09:14 -0400

    Update setup-local.sh

--- .devcontainer/LOCAL/setup-local.sh
+++ .devcontainer/LOCAL/setup-local.sh
@@ -2,5 +2,5 @@
 
 # Clone my dotfiles repo and run the setup script to install all the things
 
-git clone https://github.com/jreslock/dotfiles.git $HOME/dotfiles
-$HOME/dotfiles/setup
+# git clone https://github.com/jreslock/dotfiles.git $HOME/dotfiles
+# $HOME/dotfiles/setup
